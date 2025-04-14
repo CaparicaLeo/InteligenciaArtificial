5ee6921aa1db3f5ec773ecf1eebd56eabfa54a96
CaparicaLeo <leo_caparica@icloud.com>
2025-04-14 14:20:13 -0300

    fixing grafo

--- Trabalho1/main.py
+++ Trabalho1/main.py
@@ -1,20 +1,21 @@
 grafo = {
-    "Foz do Iguaçu": [("Cascavel", 125)],
-    "Cascavel": [("Foz do Iguaçu", 125), ("Campo Mourão", 130), ("Guarapuava", 200)],
-    "Campo Mourão": [("Cascavel", 130), ("Maringá", 90)],
-    "Maringá": [("Campo Mourão", 90), ("Mandaguari", 35)],
-    "Mandaguari": [("Maringá", 35), ("Arapongas", 25)],
-    "Arapongas": [("Mandaguari", 25), ("Apucarana", 20), ("Londrina", 30)],
-    "Apucarana": [("Arapongas", 20), ("Jandaia do Sul", 20), ("Ponta Grossa", 205)],
-    "Jandaia do Sul": [("Apucarana", 20), ("Mandaguari", 10), ("Guarapuava", 220)],
-    "Guarapuava": [("Jandaia do Sul", 220), ("Cascavel", 200), ("Irati", 90), ("Ponta Grossa", 150)],
-    "Irati": [("Guarapuava", 90), ("Curitiba", 135)],
-    "Curitiba": [("Irati", 135), ("Ponta Grossa", 110)],
-    "Ponta Grossa": [("Curitiba", 110), ("Guarapuava", 150), ("Apucarana", 205), ("Ourinhos", 275)],
-    "Ourinhos": [("Ponta Grossa", 275), ("Londrina", 125)],
-    "Londrina": [("Ourinhos", 125), ("Arapongas", 30)]
+    "Foz do Iguaçu": [("Cascavel", 125)], #Check
+    "Cascavel": [("Foz do Iguaçu", 125), ("Campo Mourão", 150), ("Guarapuava", 200)], #Check
+    "Campo Mourão": [("Cascavel", 150), ("Maringá", 90), ("Guarapuava", 175)], #Check
+    "Maringá": [("Campo Mourão", 90), ("Mandaguari", 35)], #Check
+    "Mandaguari": [("Maringá", 35), ("Arapongas", 25), ("Jandaia do Sul", 10)], #Check  
+    "Arapongas": [("Mandaguari", 25), ("Apucarana", 20), ("Londrina", 30)], #Check
+    "Apucarana": [("Arapongas", 20), ("Jandaia do Sul", 20), ("Ponta Grossa", 205)],#Check
+    "Jandaia do Sul": [("Apucarana", 20), ("Mandaguari", 10), ("Guarapuava", 220)], #Check
+    "Guarapuava": [("Jandaia do Sul", 220), ("Cascavel", 200), ("Irati", 90), ("Ponta Grossa", 150), ("Campo Mourão", 175)], #Check
+    "Irati": [("Guarapuava", 90), ("Curitiba", 135)],#Check
+    "Curitiba": [("Irati", 135), ("Ponta Grossa", 110)],#Check
+    "Ponta Grossa": [("Curitiba", 110), ("Guarapuava", 150), ("Apucarana", 205), ("Ourinhos", 275)], #Check
+    "Ourinhos": [("Ponta Grossa", 275), ("Londrina", 125)], #Check
+    "Londrina": [("Ourinhos", 125), ("Arapongas", 30)] #Check
 }
 
+#Heuristica Partindo de Curitiba
 heuristica = {  
     "Foz do Iguaçu": 400,
     "Cascavel": 320,
@@ -56,6 +57,6 @@ def a_estrela(inicio, objetivo):
 
     return None, float('inf')
 
-caminho, custo = a_estrela("Ourinhos", "Jandaia do Sul")
+caminho, custo = a_estrela("Curitiba", "Campo Mourão")
 print("Melhor caminho:", caminho)
 print("Custo total:", custo)
